ded79bb2391486c0cd9539175a6112918900a55c
Joshua <jcmathews2@wisc.edu>
2022-09-08 00:42:14 -0400

    Fill readme, remove ball acc.

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -73,37 +73,6 @@ PlayMode::PlayMode() {
 	//  make yourself a script that spits out the code that you paste in here
 	//   and check that script into your repository.
 
-	//Also, *don't* use these tiles in your game:
-
-	// { //use tiles 0-16 as some weird dot pattern thing:
-	// 	std::array< uint8_t, 8*8 > distance;
-	// 	for (uint32_t y = 0; y < 8; ++y) {
-	// 		for (uint32_t x = 0; x < 8; ++x) {
-	// 			float d = glm::length(glm::vec2((x + 0.5f) - 4.0f, (y + 0.5f) - 4.0f));
-	// 			d /= glm::length(glm::vec2(4.0f, 4.0f));
-	// 			distance[x+8*y] = uint8_t(std::max(0,std::min(255,int32_t( 255.0f * d ))));
-	// 		}
-	// 	}
-	// 	for (uint32_t index = 0; index < 16; ++index) {
-	// 		PPU466::Tile tile;
-	// 		uint8_t t = uint8_t((255 * index) / 16);
-	// 		for (uint32_t y = 0; y < 8; ++y) {
-	// 			uint8_t bit0 = 0;
-	// 			uint8_t bit1 = 0;
-	// 			for (uint32_t x = 0; x < 8; ++x) {
-	// 				uint8_t d = distance[x+8*y];
-	// 				if (d > t) {
-	// 					bit0 |= (1 << x);
-	// 				} else {
-	// 					bit1 |= (1 << x);
-	// 				}
-	// 			}
-	// 			tile.bit0[y] = bit0;
-	// 			tile.bit1[y] = bit1;
-	// 		}
-	// 		ppu.tile_table[index] = tile;
-	// 	}
-	// }
 
 	for (uint32_t x = 0; x < PPU466::BackgroundWidth; x++) {
 		for (uint32_t y = 0; y < PPU466::BackgroundHeight; y++) {
@@ -116,27 +85,6 @@ PlayMode::PlayMode() {
 
 	}
 
-	//use sprite 32 as a "player":
-	// ppu.tile_table[32].bit0 = {
-	// 	0b01111110,
-	// 	0b11111111,
-	// 	0b11111111,
-	// 	0b11111111,
-	// 	0b11111111,
-	// 	0b11111111,
-	// 	0b11111111,
-	// 	0b01111110,
-	// };
-	// ppu.tile_table[32].bit1 = {
-	// 	0b00000000,
-	// 	0b00000000,
-	// 	0b00011000,
-	// 	0b00100100,
-	// 	0b00000000,
-	// 	0b00100100,
-	// 	0b00000000,
-	// 	0b00000000,
-	// };
 	ppu.tile_table[0] = brick_tile->tile;
 	ppu.tile_table[1] = back_tile->tile;
 	ppu.tile_table[2] = l_tile->tile;
@@ -147,21 +95,6 @@ PlayMode::PlayMode() {
 	ppu.tile_table[32] = player_tile->tile;
 	ppu.tile_table[33] = ball_tile->tile;
 
-	//makes the outside of tiles 0-16 solid:
-	// ppu.palette_table[0] = {
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	// };
-
-	//makes the center of tiles 0-16 solid:
-	// ppu.palette_table[1] = {
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	// 	glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	// };
 
 	// used for the background:
 	ppu.palette_table[0] = {
@@ -256,18 +189,13 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 
 void PlayMode::update(float elapsed) {
 
-	//slowly rotates through [0,1):
-	// (will be used to set background color)
-	// background_fade += elapsed / 10.0f;
-	// background_fade -= std::floor(background_fade);
 	static float PlayerSpeed = 60.0f;
 
-	static float BallSpeed = 240.0f;
+	static float BallSpeed = 200.0f;
 
 	static float BallSpeedX = 60.0f;
 	static float BallSpeedY = 60.0f;
 
-	BallSpeed += 0.1;
 
 	auto dist = [=](glm::vec2 a, glm::vec2 b) {
 		float x = b.x - a.x;
@@ -343,26 +271,6 @@ void PlayMode::update(float elapsed) {
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	//--- set ppu state based on game state ---
 
-	//background color will be some hsv-like fade:
-	// ppu.background_color = glm::u8vec4(
-	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
-	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 1.0f / 3.0f) ) ) ))),
-	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 2.0f / 3.0f) ) ) ))),
-	// 	0xff
-	// );
-
-	//tilemap gets recomputed every frame as some weird plasma thing:
-	//NOTE: don't do this in your game! actually make a map or something :-)
-	// for (uint32_t y = 0; y < PPU466::BackgroundHeight; ++y) {
-	// 	for (uint32_t x = 0; x < PPU466::BackgroundWidth; ++x) {
-	// 		//TODO: make weird plasma thing
-	// 		ppu.background[x+PPU466::BackgroundWidth*y] = ((x+y)%16);
-	// 	}
-	// }
-
-	//background scroll:
-	// ppu.background_position.x = int32_t(-0.5f * player_at.x);
-	// ppu.background_position.y = int32_t(-0.5f * player_at.y);
 
 	//player sprite:
 	ppu.sprites[0].x = int8_t(player_at.x);
@@ -376,15 +284,6 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	ppu.sprites[1].index = 33;
 	ppu.sprites[1].attributes = 6;
 
-	//some other misc sprites:
-	// for (uint32_t i = 1; i < 63; ++i) {
-	// 	float amt = (i + 2.0f * background_fade) / 62.0f;
-	// 	ppu.sprites[i].x = int8_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
-	// 	ppu.sprites[i].y = int8_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
-	// 	ppu.sprites[i].index = 32;
-	// 	ppu.sprites[i].attributes = 6;
-	// 	if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
-	// }
 
 	//--- actually draw ---
 	ppu.draw(drawable_size);
--- README.md
+++ README.md
@@ -1,22 +1,28 @@
-# (TODO: your game's title)
+# Rick Breaker ![link](processing/player2.png)
 
-Author: (TODO: your name)
+Author: Joshua Mathews
 
-Design: (TODO: In two sentences or fewer, describe what is new and interesting about your game.)
+Design: Brick breaker but you control Rick (the Chosen One) and try to evade the ball.
 
 Screen Shot:
 
-![Screen Shot](screenshot.png)
+![Screen Shot](screenshot2.png)
 
 How Your Asset Pipeline Works:
 
-(TODO: describe the steps in your asset pipeline, from source files to tiles/backgrounds/whatever you upload to the PPU466.)
+Tiles are created as 8x8 .png images with 3 colors by the author. The process.py python script then processes these image (specified in script) to a binary tile format required by the PPU by setting index 1 to 3 for the first 3 colors it sees and 0 for white and then stores it in the dist/ folder. Palettes are hard coded into the code to allow for easier color scheme changes. The PlayMode code loads these assets from the binary files and sets them in the tile table.
 
-(TODO: make sure the source files you drew are included. You can [link](your/file.png) to them to be a bit fancier.)
+Assets:
+* Rick: ![link](processing/player2.png)
+* Rick Mortem: ![link](processing/death.png)
+* Brick: ![link](processing/brick.png)
+* Ball: ![link](processing/ball.png)
+* End screen text: ![link](processing/l.png)![link](processing/o.png)![link](processing/s.png)![link](processing/e.png)![link](processing/r.png)
+
+
 
 How To Play:
 
-(TODO: describe the controls and (if needed) goals/strategy.)
+Arrow keys to move. The goal is to survive as long as possible. The ball aims towards you when it hits the top or bottom. It can't move through bricks but you can (definitely a game mechanic, not a bug). No win screens, only personal satisfaction.
 
 This game was built with [NEST](NEST.md).
-
